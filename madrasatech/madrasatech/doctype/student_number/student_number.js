2215b3e100477c935c9df2fd302c6b6e68d1cd79
Omar Taha <omartaha.tech7@gmail.com>
2022-05-17 14:39:26 +0300

    Test the student numbers

--- madrasatech/madrasatech/doctype/student_number/student_number.js
+++ madrasatech/madrasatech/doctype/student_number/student_number.js
@@ -2,33 +2,19 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Student Number', {
-	setup: function (frm) {
-		if (frm.is_new())
-			frm.set_df_property("generate_numbers", "hidden");
-	},
 	refresh: function (frm) {
-
 		if (!frm.is_new())
 			frm.set_df_property("generate_numbers", "hidden");
 	},
 	generate_numbers: function (frm) {
-		frm.save();
-		frm.refresh();
-		cur_frm.clear_table("students");
 		frm.events.fill_students(frm);
-		// frm.refresh_field("students");
-		frm.save();
-
-		frm.refresh();
-		frm.reload_doc();
 
 	},
 
 
 	fill_students: function (frm) {
 		return frappe.call({
-
-			method: 'erpnext.education.doctype.student_number.student_number.fill_students',
+			method: 'madrasatech.madrasatech.doctype.student_number.student_number.fill_students',
 			args: {
 				st_no: frm.doc.name,
 				std_group: frm.doc.student_group,
@@ -37,20 +23,10 @@ frappe.ui.form.on('Student Number', {
 				student_category: frm.doc.student_category
 			},
 			callback: function (msg) {
-				frm.save();
-				frm.refresh_field("students");
+				refresh_field("students");
 				frm.refresh();
-				if (r.docs[0].students) {
-					frm.save();
-					frm.refresh();
-
-				}
-
+				frm.save();
 			}
-
 		});
-
 	}
-
-
 });
--- madrasatech/madrasatech/doctype/student_number/student_number.py
+++ madrasatech/madrasatech/doctype/student_number/student_number.py
@@ -11,50 +11,42 @@ class StudentNumber(Document):
 
 
 @frappe.whitelist()
-def get_students_list(st_no, std_group, program, academic_term, student_category):
+def get_students_list(std_group, program):
     filters = [program]
     cond = ''
     if std_group:
         cond += ' and sg.name=%s '
         filters.append(std_group)
-    if academic_term:
-        cond += ' and sg.academic_term=%s '
-        filters.append(academic_term)
-    if student_category:
-        cond += ' and sg.student_category=%s '
-        filters.append(student_category)
 
-    return frappe.db.sql("""select sgs.student,sg.program, sg.academic_year, sg.name, sg.academic_term , sg.student_category
+    return frappe.db.sql("""select sgs.student, sgs.student_name, sg.program, sg.student_group_name
 			from `tabStudent Group` sg
 			LEFT JOIN  `tabStudent Group Student` sgs 
 			ON sg.name=sgs.parent
 			where sg.program=%s {0}
-			
 			""".format(cond), filters, as_dict=True)
 
 
 @frappe.whitelist()
-def fill_students(st_no, std_group=None, program=None, academic_term=None, student_category=None):
+def fill_students(st_no, std_group=None, program=None):
     students = get_students_list(
-        st_no, std_group, program, academic_term, student_category)
+        std_group, program)
 
     if not students:
-        frappe.msgprint(_("No students for the mentioned type"))
+        frappe.msgprint(_("No students for the mentioned Students group"))
     i = 1
     for std in students:
         std['exam_number'] = i
-        std['secrete_number'] = random_string(10)
+        std['secret_number'] = random_string(10)
         i += 1
-    # frappe.msgprint(frappe.as_json(students))
-    # return students
 
-    st_gr = frappe.get_doc("Student Number", st_no)
-    st_gr.students = None
+    student_number = frappe.get_doc("Student Number", st_no)
+    student_number.students = None
     for d in students:
-        row = st_gr.append('students', {})
-        row.student = d.student
-        row.secrete_number = d.secrete_number
-        row.exam_number = d.exam_number
-    # frappe.msgprint(frappe.as_json(st_gr))
-    st_gr.save()
-    st_gr.reload()
+        if d.student not in students:
+            row = student_number.append('students', {})
+            row.student_group = d.student_group_name
+            row.student = d.student
+            row.student_name = d.student_name
+            row.secret_number = d.secret_number
+            row.exam_number = d.exam_number
+    student_number.save()
