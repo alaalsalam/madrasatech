73e21a74152e45fef4db9a6d0ca1a47dcdb41f1e
alaalsalam <alaalsalam101@gmail.com>
2022-05-18 14:38:02 +0300

    add assesssment result tool html file

--- madrasatech/madrasatech/doctype/assessment_plan_program/assessment_plan_program.py
+++ madrasatech/madrasatech/doctype/assessment_plan_program/assessment_plan_program.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2022, MadrasaTech TEAM and contributors
 # For license information, please see license.txt
 
+from itertools import count
 import frappe
 from frappe import _
 from frappe.model.document import Document
+from madrasatech.madrasatech.api import get_student_group_students
 
 class AssessmentPlanProgram(Document):
 	def validate(self):
@@ -67,3 +69,37 @@ class AssessmentPlanProgram(Document):
 			(self.program),
 			as_dict=1,
 		)
+	
+	def on_submit(self):
+		self.create_assessment_result()
+
+	def create_assessment_result(self):
+		# student = frappe.get_doc("Student", self.student)
+		# course_list = [course.course for course in self.courses]
+		# for course_name in course_list:
+		# 	student.enroll_in_course(
+		# 		course_name=course_name, program_enrollment=self.name, enrollment_date=self.enrollment_date
+		# 	)
+		student_list = get_student_group_students(self.student_group)
+		count = 0
+		# frappe.throw(_("You count {}").format(student_list.student))
+		for student in student_list:
+			# pass
+			# frappe.throw(_("You  {}").format(i))
+			# frappe.msgprint(_("Result already Submitted{}").format())
+			doc = frappe.new_doc('Assessment Result Program')
+			doc.assessment_plan = self.name
+			doc.student = student.student
+			doc.student_group = self.student_group
+			doc.insert(ignore_permissions=True, ignore_links=True, ignore_if_duplicate=True,ignore_mandatory=True)
+			count +=1
+
+			frappe.msgprint(_("Result already Submitted{}").format(count))			
+	
+	# @frappe.whitelist()
+	# def get_courses(self):
+	# 	return frappe.db.sql(
+	# 		"""select course from `tabProgram Course` where parent = %s and required = 1""",
+	# 		(self.program),
+	# 		as_dict=1,
+	# 	)
