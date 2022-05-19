928dc8b69572a34dae649adebd199d4b1ca17b2f
samia nasser <alshehri2engsam@gmail.com>
2022-05-19 10:07:46 +0000

    Assessmnt schdual

---
+++ madrasatech/madrasatech/doctype/assessment_schedule/assessment_schedule.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, MadrasaTech TEAM and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Assessment Schedule', {
+	// refresh: function(frm) {
+
+	// }
+});
